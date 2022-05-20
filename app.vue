fecc050ef679547b93d3d790edeb207154161241
Tsuev <tsuevjudoka@gmail.com>
2022-05-21 01:46:17 +0300

    BismiLlah first commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script>
+</script> 
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script setup>
+    let message = 'Is course platform'
+</script>
+
+<style lang='scss'>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+    
+    h1 {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+    }
+</style>
